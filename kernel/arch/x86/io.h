ab4614232f5b780e8f46b7c93c2e460899d7fd1a
Neil Chen <neilchen1998@gmail.com>
2026-08-22 16:56:45 -0500

    fixed potential issues in functions in io.h

--- kernel/arch/x86/io.h
+++ kernel/arch/x86/io.h
@@ -3,32 +3,42 @@
 
 #include <stdint.h>
 
+// @brief Writes an 8-bit value to the specified I/O port.
+//
+// @param port I/O port address to write to.
+// @param value 8-bit value to write to the port.
 static inline void outb(uint16_t port, uint8_t value)
 {
-    __asm__ volatile (
-        "outb %0, %1"
-        :
-        : "a"(value), "Nd"(port)
-    );
+    __asm__ volatile("outb %0, %1" : : "a"(value), "Nd"(port) : "memory");
 }
 
-static inline unsigned char inb(uint16_t port)
+// @bried Writes an 8-bit value to the specified I/O port.
+//
+// @para port I/O port address to read from.
+// @return The 8-bit value read from the port.
+static inline uint8_t inb(uint16_t port)
 {
-    unsigned char value;
-    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
+    uint8_t value;
+    __asm__ volatile("inb %1, %0" : "=a"(value) : "Nd"(port) : "memory");
     return value;
 }
 
+// @bried Saves the current CPU interrupt flags and disable interrupts.
+//
+// @return The CPU interrupt flags captured before interrupts were disabled.
 static inline uint32_t irq_save(void)
 {
     uint32_t flags;
-    __asm__ volatile("pushf; pop %0; cli": "=r"(flags));
+    __asm__ volatile("pushf; pop %0; cli" : "=r"(flags) : : "memory");
     return flags;
 }
 
-static inline uint32_t irq_restore(uint32_t flags)
+// @bried Restores the CPU interrupt flags, including the previous interrupt state.
+//
+// @para flags Interrupt flags previously returned by irq_save().
+static inline void irq_restore(uint32_t flags)
 {
-    __asm__ volatile("push %0; popf" :: "r"(flags));
+    __asm__ volatile("push %0; popf" ::"r"(flags) : "memory");
 }
 
-#endif  // KERNEL_ARCH_X86_IO_H
+#endif // KERNEL_ARCH_X86_IO_H
