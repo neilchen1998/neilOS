333ab348ead566dc19ba337d7b2fe4044655b6a0
Neil Chen <neilchen1998@gmail.com>
2026-08-29 16:32:34 -0500

    refactored the test assert functions

--- kernel/tests/common/test_assert.c
+++ kernel/tests/common/test_assert.c
@@ -1,15 +1,77 @@
 #include "test_assert.h"
 
 #include "drivers/video/vga.h"
+#include "stddef.h"
 
-void test_assert(const char* msg, bool condition)
+static void expect_fail(const char* msg)
+{
+    vga_set_color(VGA_RED, VGA_BLACK);
+    fvga_write("ASSERT FAILED: ");
+    fvga_write(msg);
+    fvga_write("\n");
+    vga_set_color(VGA_WHITE, VGA_BLACK);
+}
+
+void EXPECT_TRUE(const char* msg, bool condition)
 {
     if (!condition)
     {
-        vga_set_color(VGA_RED, VGA_BLACK);
-        fvga_write("ASSERT FAILED: ");
-        fvga_write(msg);
-        fvga_write("\n");
-        vga_set_color(VGA_WHITE, VGA_BLACK);
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_FALSE(const char* msg, bool condition)
+{
+    if (condition)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_EQ(const char* msg, int val1, int val2)
+{
+    if (val1 != val2)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_NE(const char* msg, int val1, int val2)
+{
+    if (val1 == val2)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_LT(const char* msg, int val1, int val2)
+{
+    if (val1 >= val2)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_LE(const char* msg, int val1, int val2)
+{
+    if (val1 > val2)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_GT(const char* msg, int val1, int val2)
+{
+    if (val1 <= val2)
+    {
+        expect_fail(msg);
+    }
+}
+
+void EXPECT_GE(const char* msg, int val1, int val2)
+{
+    if (val1 < val2)
+    {
+        expect_fail(msg);
     }
 }
--- kernel/tests/common/test_assert.h
+++ kernel/tests/common/test_assert.h
@@ -1,10 +1,75 @@
 #ifndef KERNEL_TESTS_COMMON_TEST_ASSERT_H
 #define KERNEL_TESTS_COMMON_TEST_ASSERT_H
 
-// @brief Checks a test condition and reports an assertion failure.
+#include <stdbool.h>
+
+// @brief Expects a condition to be true.
 //
-// @param msg Description of the assertion.
-// @param condition Condition that must evaluate to true.
-void test_assert(const char* msg, bool condition);
+// Reports an assertion failure if the condition evaluates to false.
+// @param msg       Description of the expectation.
+// @param condition Condition that is expected to be true.
+void EXPECT_TRUE(const char* msg, bool condition);
+
+// @brief Expects a condition to be false.
+//
+// Reports an assertion failure if the condition evaluates to true.
+//
+// @param msg       Description of the expectation.
+// @param condition Condition that is expected to be false.
+void EXPECT_FALSE(const char* msg, bool condition);
+
+// @brief Expects two integer values to be equal.
+//
+// Reports an assertion failure if the two values are not equal.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_EQ(const char* msg, int val1, int val2);
+
+// @brief Expects two integer values to be different.
+//
+// Reports an assertion failure if the two values are equal.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_NE(const char* msg, int val1, int val2);
+
+// @brief Expects the first integer value to be less than the second.
+//
+// Reports an assertion failure if val1 is greater than or equal to val2.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_LT(const char* msg, int val1, int val2);
+
+// @brief Expects the first integer value to be less than or equal to the second.
+//
+// Reports an assertion failure if val1 is greater than val2.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_LE(const char* msg, int val1, int val2);
+
+// @brief Expects the first integer value to be greater than the second.
+//
+// Reports an assertion failure if val1 is less than or equal to val2.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_GT(const char* msg, int val1, int val2);
+
+// @brief Expects the first integer value to be greater than or equal to the second.
+//
+// Reports an assertion failure if val1 is less than val2.
+//
+// @param msg  Description of the expectation.
+// @param val1 First value to compare.
+// @param val2 Second value to compare.
+void EXPECT_GE(const char* msg, int val1, int val2);
 
 #endif // KERNEL_TESTS_COMMON_TEST_ASSERT_H
