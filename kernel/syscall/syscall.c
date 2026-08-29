cecd1ca3b6e5b74d4296a7d1e1e1806b45a831d1
Neil Chen <neilchen1998@gmail.com>
2026-08-29 18:08:11 -0500

    added cases for syscall

--- kernel/syscall/syscall.c
+++ kernel/syscall/syscall.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 #include "arch/x86/idt.h"
+#include "drivers/timer/pit.h"
 #include "scheduler/scheduler.h"
 
 struct registers* syscall_dispatch(struct registers* regs)
@@ -14,11 +15,95 @@ struct registers* syscall_dispatch(struct registers* regs)
 
     switch (regs->eax)
     {
+    case SYS_EXIT:
+    {
+        return scheduler_exit(regs);
+    }
     case SYS_YIELD:
     {
         return scheduler_schedule(regs);
     }
-
+    // case SYS_GETPID:
+    // {
+    //     return regs;
+    // }
+    case SYS_GET_TICKS:
+    {
+        regs->eax = pit_get_ticks();
+        return regs;
+    }
+    // case SYS_SLEEP:
+    // {
+    //     return regs;
+    // }
+    // case SYS_WRITE:
+    // {
+    //     return regs;
+    // }
+    // case SYS_READ:
+    // {
+    //     return regs;
+    // }
+    // case SYS_SBRK:
+    // {
+    //     return regs;
+    // }
+    // case SYS_MMAP:
+    // {
+    //     return regs;
+    // }
+    // case SYS_MUNMAP:
+    // {
+    //     return regs;
+    // }
+    // case SYS_OPEN:
+    // {
+    //     return regs;
+    // }
+    // case SYS_CLOSE:
+    // {
+    //     return regs;
+    // }
+    // case SYS_READ_FILE:
+    // {
+    //     return regs;
+    // }
+    // case SYS_WRITE_FILE:
+    // {
+    //     return regs;
+    // }
+    // case SYS_SEEK:
+    // {
+    //     return regs;
+    // }
+    // case SYS_STAT:
+    // {
+    //     return regs;
+    // }
+    // case SYS_FORK:
+    // {
+    //     return regs;
+    // }
+    // case SYS_EXEC:
+    // {
+    //     return regs;
+    // }
+    // case SYS_WAIT:
+    // {
+    //     return regs;
+    // }
+    // case SYS_GET_TIME:
+    // {
+    //     return regs;
+    // }
+    // case SYS_GET_MEMINFO:
+    // {
+    //     return regs;
+    // }
+    // case SYS_COMPLETE:
+    // {
+    //     return regs;
+    // }
     default:
     {
         regs->eax = (uint32_t)-1;
--- kernel/tests/syscall/test_syscall.c
+++ kernel/tests/syscall/test_syscall.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "arch/x86/idt.h"
+#include "drivers/timer/pit.h"
 #include "drivers/video/vga.h"
 #include "stdbool.h"
 #include "syscall/syscall.h"
@@ -18,7 +19,17 @@ bool syscall_test(void)
 
     // Case (i): null register
     result = syscall_dispatch(NULL);
-    test_assert("Reject NULL register state", result != NULL);
+    EXPECT_FALSE("Reject NULL register state", result);
+
+    // Case (iv): tick count
+    uint32_t before = pit_get_ticks();
+    regs.eax = SYS_GET_TICKS;
+    result = syscall_dispatch(&regs);
+    uint32_t after = pit_get_ticks();
+
+    EXPECT_TRUE("syscall_dispatch returns regs", result == &regs);
+    EXPECT_GE("SYS_GET_TICKS returns a valid tick count", regs.eax, before);
+    EXPECT_LE("SYS_GET_TICKS returns a valid tick count", regs.eax, after);
 
     fvga_write("\n=== SYSCALL TEST COMPLETE ===\n");
 
